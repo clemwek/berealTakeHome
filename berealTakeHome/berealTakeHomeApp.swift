f1907f985a113cbf679476ddba850dd73deddd55
Clement <clemwek@gmail.com>
2025-04-27 11:00:45 +0300

    Initial Commit

---
+++ berealTakeHome/berealTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  berealTakeHomeApp.swift
+//  berealTakeHome
+//
+//  Created by Clement  Wekesa on 4/27/25.
+//
+
+import SwiftUI
+
+@main
+struct berealTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
